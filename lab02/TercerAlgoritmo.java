6c260df3a3c588b1c3f4d3c252af28a040811990
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-05-12 12:20:51 -0500

    Completando los ejercicios

--- lab02/TercerAlgoritmo.java
+++ lab02/TercerAlgoritmo.java
@@ -1,17 +1,19 @@
 public class TercerAlgoritmo {
     public static void main(String[] args) {
-        trianguloRecursivo(5);
+        int base = 5;
+        trianguloRecursivo(base, 1);
     }
 
-    public static void trianguloRecursivo(int base) {
-        if (base == 1){
-            System.out.println("*");
-        }
-        else {
+    public static void trianguloRecursivo(int base, int var) {
+        if (base == var){
             for (int i=0; i<base; i++)
+                System.out.print("*");        
+        } 
+        else {
+            for (int i=0; i<var; i++)
                 System.out.print("*");
             System.out.println();
-            trianguloRecursivo(base - 1);        
+            trianguloRecursivo(base, var+1);        
         }
     }
 }
