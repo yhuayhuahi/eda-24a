cec2248987612204b07cf9d828f904e32885971a
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-05-25 22:31:55 -0500

    Se crea la clase Operaciones

---
+++ lab04/Operaciones.java
@@ -0,0 +1,32 @@
+public class Operaciones {
+    
+    public Operaciones() {}
+
+    public Integer suma(Integer num1, Integer num2) {
+        return num1 + num2;
+    }
+    public Double suma(Double num1, Double num2) {
+        return num1 + num2;
+    }
+
+    public Integer resta(Integer num1, Integer num2) {
+        return num1 - num2;
+    }
+    public Double resta(Double num1, Double num2) {
+        return num1 - num2;
+    }
+
+    public Integer multiplicacion(Integer num1, Integer num2) {
+        return num1 * num2;
+    }
+    public Double multiplicacion(Double num1, Double num2) {
+        return num1 * num2;
+    }
+
+    public Integer division(Integer num1, Integer num2) {
+        return num1 / num2;
+    }
+    public Double division(Double num1, Double num2) {
+        return num1 / num2;
+    }
+}
